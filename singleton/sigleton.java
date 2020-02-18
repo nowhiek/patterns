104e4bbffa23d547cceb385d4bd72f7f0998fb5b
nowhiek <ev.teechart@gmail.com>
2020-02-18 14:28:47 +0300

    Singleton

--- singleton/sigleton.java
+++ singleton/sigleton.java
@@ -12,7 +12,7 @@ class Singleton {
     }
 }
 
-public class SingletonTest {//тест
+public class SingletonTest {
     public static void main(String[] args){
         Singleton singelton = Singleton.getInstance();
         singelton.setUp();
