82dfdbe42285d0217bc5c215b81f3b8090363997
nowhiek <ev.teechart@gmail.com>
2020-02-21 12:54:28 +0300

    Chain of responsibility

---
+++ Behavioral Design Patterns/chain-of-responsibility/chain_of_resposibility.java
@@ -0,0 +1,33 @@
+interface Payment {
+    void setNext(Payment payment);
+    void pay();
+}
+
+class VisaPayment implements Payment {
+    private Payment payment;
+    public void setNext(Payment payment) {
+        this.payment = payment;
+    }
+    public void pay() {
+        System.out.println("Visa Payment");
+    }
+}
+
+class PayPalPayment implements Payment {
+    private Payment payment;
+    public void setNext(Payment payment) {
+        this.payment = payment;
+    }
+    public void pay() {
+        System.out.println("PayPal Payment");
+    }
+}
+
+public class ChainofResponsibilityTest {
+    public static void main(String[] args) {
+        Payment visaPayment = new VisaPayment();
+        Payment payPalPayment = new PayPalPayment();
+        visaPayment.setNext(payPalPayment);
+        visaPayment.pay();
+    }
+}
